a9b3953549eec9f69bad08b09182941142e53c21
Rafael Caixeta <caixetadev@gmail.com>
2021-09-14 10:50:24 -0300

    algumas mudança

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,27 @@ form.addEventListener('submit', (event) => {
 
       `
     } else {
-      document.querySelector('.container').innerHTML = `
+      const container = document.querySelector('.container')
+      container.innerHTML = `
       
       <h1>${user.login}</h1>
       <img src="${user.avatar_url}">
-      <p>${user.name}</p>
+      <h2>${user.name}</h2>
+      <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-people">
+      <path fill-rule="evenodd" d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5. 01 0 00-2.56-3.012A3 3 0 0011 4z"></path>
+      </svg>
+      
+      <span>Seguidores ${user.followers}</span>
+      <span>Seguindo ${user.following}</span><br>
+      <svg class="octicon octicon-location" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"></path></svg>
+      <span>${user.location}</span><br>
+      <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-link">
+      <path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path>
+      </svg>
+
+      <span class="repo">Repositório ${user.public_repos}</span>
+      <span class="repo">Gists ${user.public_gists}</span>
+
       
       `
     }
